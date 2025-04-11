d8024f0511cc2705805a0bd065d4145680bb09f9
Gabriel Nascimento <166341117+uKuroo@users.noreply.github.com>
2025-04-11 13:43:34 -0300

    Create Car.java

---
+++ Vehicle/Car.java
@@ -0,0 +1,12 @@
+public class Car extends Vehicle
+{
+    private int doors;
+
+    public Car(String brand, int year, int fuel, int doors){
+        this.brand = brand;
+        this.year = year;
+        this.fuel = fuel;
+        this.doors = doors;
+        this.broken = false;
+    }
+}
